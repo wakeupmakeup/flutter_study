0b5f4c05bddc076b233247ed93f8699361ec6c21
wakeupmakeup <zerostar998@naver.com>
2024-02-27 01:58:32 +0900

    [ADD] Button Section

--- lib/main.dart
+++ lib/main.dart
@@ -10,38 +10,21 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // 색상 정보를 쓸때 Colors.색상.색조
-        //커스텀 컬러
         backgroundColor: Color(0xFF181818),
-
-        // Colum은 하나의 child만 가지지 않음 대신 children이라는 List가 존재함.
-        // 여기서 row는 한 섹션을 말함. 강의를 보면 한 Row에 하나의 텍스트와 또 하나의 텍스트가 있음.
-        // 다른걸 올리기 위해선 Column을 사용해야한다.
-        // 즉, Row하나를 만들고 텍스트가 들어있는 2개의 Colume을 만든다.
         body: Padding(
-          // padding에서 required하고 있는데 타입을 보면 EdgeInsetsGeometry타입을 요구하고 있다.
-          // 이럴때는 아래와 같이 입력한다.
-
-          // all이라는 것은 상하좌우를 뜻한다.
-          // 상하좌우 하나만 하고 싶을때는 only를 쓴다.
-          // padding: EdgeInsets.all(10),
-
-          // 이것은 가로 세로를 뜻한다.
-          padding: EdgeInsets.symmetric(horizontal: 40),
+          padding: EdgeInsets.symmetric(
+            horizontal: 40,
+          ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
               ),
-              // 보면 알겠지만 Row도 children List가 있어야 한다.
-              // Row나 Colume 모두 children List가 있어야 한다고 생각하자.
-              // 마지막에 콤마를 꼭 기억해라
               Row(
-                // 수평방향
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Column(
-                    // 수평방향 (오른쪽 정렬))
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
                       Text(
@@ -59,6 +42,62 @@ class App extends StatelessWidget {
                         ),
                       ),
                     ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                // 변수가 아님을 알려줄때 이렇게 사용.
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Row(
+                children: [
+                  // 컨테이너는 div 같은것이고 하나의 child를 가진다.
+                  Container(
+                    // 단순하게 backgroundcolor로 하는게 아니라 데코레이션이라는 것을 사용해야함.
+                    // 이외에도 border radius 작업 등에도 사용됨
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(
+                        45,
+                      ),
+                    ),
+                    // 지금 보면 패딩 값을 줄때 child에 주는 것을 알 수 있음.
+                    // child에 줘야 컨테이너 안에 넣을 텍스트에 직접 적용된다.
+
+                    child: Padding(
+                      // padding 값을 넣을때 symmetric을 넣고 가로 세로 값을 입력하면 된다.
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 20,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(
+                          fontSize: 22,
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
