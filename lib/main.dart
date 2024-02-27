10a1665450932495783460cd683d74688c9949be
wakeupmakeup <zerostar998@naver.com>
2024-02-27 21:23:51 +0900

    [ADD} Cards

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ void main() {
   runApp(const App());
 }
 
-// 최종 코드
 class App extends StatelessWidget {
   const App({super.key});
 
@@ -22,7 +21,7 @@ class App extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(
-                height: 80,
+                height: 60,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
@@ -75,9 +74,6 @@ class App extends StatelessWidget {
               const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  // 이렇게 하면 정말 간단하게 짤 수 있음.
-                  // 여기서는 값들을 미리 알 수 있기 때문에 const가 붙는 것임.
-                  // 지우면 오류는 나지는 않는다.
                   Button(
                     text: 'Transfer',
                     bgColor: Colors.amber,
@@ -89,6 +85,88 @@ class App extends StatelessWidget {
                     textColor: Colors.white,
                   ),
                 ],
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Row(
+                // Row에는 main, cross 축이 있음. main이 가로축 cross가 세로축임.
+                // 하지만 Colume에서 main축은 세로축임. 서로 반대다.
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // 여기는 투명도를 조정하지 않아 const가 자동으로 붙는 모습이다.
+                  const Text(
+                    'Wallets',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    'View All',
+                    style: TextStyle(
+                      // Opacity값을 사용할때 const로 하면 오류난다. 컴파일 할때 값을 알기가 어려워서 그럼
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFF1F2123),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Euro",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 32,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              const Text(
+                                '6 428',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              // 두 텍스트 사이 간격 맞추기
+                              const SizedBox(
+                                width: 5,
+                              ),
+
+                              Text(
+                                'EUR',
+                                style: TextStyle(
+                                  color: Colors.white.withOpacity(0.8),
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
--- lib/widgets/Button.dart
+++ lib/widgets/Button.dart
@@ -8,18 +8,6 @@ class Button extends StatelessWidget {
   final Color textColor;
 
   const Button({
-    // key에 관해서는 나중에 설명.
-    // 이렇게 final을 만들었다면 생성자 함수를 만들자.
-    // required를 사용해서 선언된 매개변수는 해당 클래스의 인스턴스를 생성할때 반드시 값을 전달해야 하는
-    // 매개변수임을 나타냄. 즉, 해당 변수에 null 값이 들어갈 수 없음.
-
-    // 인스턴스 : 객체 지향 프로그래밍에서 클래스를 기반으로 생성된 구체적 개첼르 말함.
-    // 클래스는 설계도라고 생각하면 됨, 객체의 속성, 동작을 정의함. 이 클래스를 기반으로 실제로 메모리에 할당된 객체를
-    // 인스턴스라고 함.
-
-    // 예를 들어 위 Button 클래스는 버튼을 나타내는 템플릿인데 이것을 사용해서 Button 클래스의 인스턴스를 생성하면, 실제로 사용할 수 있는 버튼 객체가 만들어짐
-
-    // code action 부분
     super.key,
     required this.text,
     required this.bgColor,
@@ -30,14 +18,11 @@ class Button extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        // 원래 하드 코딩 되어 있던 부분을 이렇게 적용시킨다.
         color: bgColor,
         borderRadius: BorderRadius.circular(
           45,
         ),
       ),
-
-      // 원래 Padding에 const가 있을때 오류가 나는데 이것은 밑에 값들이 상수 였을때 가능했던것.
       child: Padding(
         padding: const EdgeInsets.symmetric(
           horizontal: 50,
